5457039bba1d2a9babbba430648a37a338d96b9b
root <tomcopple@gmail.com>
2018-08-07 17:13:01 +0200

    Trying to fix a rogue console.log

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    console.log(req, res);
+    // console.log(req, res);
   next(createError(404));
 });
 
