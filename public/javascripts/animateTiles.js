d9e4fadfd661f06786f069b7ab24037435018f65
Tom <tomcopple@gmail.com>
2018-08-07 14:29:16 +0100

    Speeding up animateTiles

--- public/javascripts/animateTiles.js
+++ public/javascripts/animateTiles.js
@@ -6,34 +6,6 @@ $(document).ready(function() {
         }, {
             duration: timer
         });
-        timer = timer + 500;
+        timer = timer + 250;
     })
 })
-
-
-
-// var getAlbumtiles = document.getElementsByClassName('albumTiles');
-// var albumtiles = Array.prototype.map.call(getAlbumtiles, function(x) {
-//     console.log(x);
-//     return x;
-// })
-// console.log(albumtiles);
-
-// var timer = 1;
-
-// albumtiles.forEach(function(x) {
-
-//     x.style["-webkit-transition: opacity " + timer + "s ease-in"];
-//     x.style["-mox-transition: opacity " + timer + "s ease-in"];
-//     x.style["-ms-transition: opacity " + timer + "s ease-in"];
-//     x.style["-o-transition: opacity " + timer + "s ease-in"];
-//     x.style["transition: opacity " + timer + "s ease-in"];
-//     x.classList.add('load');
-    
-//     timer = timer + 0.5;
-// })
-
-// var classes = document.getElementsByClassName("klass"); // Do not use a period here!
-// var values = Array.prototype.map.call(classes, function(el) {
-//     return el.value;
-// });
